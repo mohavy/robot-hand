05d1afe80f6e8a3f584a97a86de8fc96741556b6
mohavy <41898262+mohavy@users.noreply.github.com>
2020-11-18 11:13:39 +1100

    Update Finger.h
    Added code to Finger.h. This is now the class header file.

--- Finger.h
+++ Finger.h
@@ -1,1 +1,22 @@
-class 
+/* 
+	Finger.h - Library for controlling a robotic hand.
+	Created by J. C. Strickland, November 17, 2020.
+*/
+#ifndef Finger_h
+#define Finger_h
+
+#include "Arduino.h"
+class Finger {
+  public:
+    Finger(int p1, int p2, int p3, int pos);
+    void cycle();
+  private:
+    void recieveAngles(); // Look at serial for sensor data.
+    void mapAngle(int x); // Take sensor data and map it to an angle.
+    void outputAngles();  // Output angles to servos.
+    int proxPin;
+    int middPin;
+    int distPin;   
+}
+
+#endif
